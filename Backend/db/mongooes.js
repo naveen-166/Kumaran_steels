9b86fd2186f42cee75457a78f7ac1bf2ff183720
naveen-166 <naveensrn1935@gmail.com>
2025-05-21 22:04:04 +0530

    ready

--- Backend/db/mongooes.js
+++ Backend/db/mongooes.js
@@ -1,8 +1,3 @@
-// const mongoose=require('mongoose');
-// const mongourl='mongodb+srv://naveen_166:naveen123@resumeshortlist.nh03e.mongodb.net/'
-// mongoose.connect(mongourl).then(()=>console.log("Database Connected")).catch(err=>{console.log(err)});
-
-
 const mongoose=require('mongoose');
 const mongourl = 'mongodb+srv://naveen_166:naveen123@resumeshortlist.nh03e.mongodb.net/kumaransteels?retryWrites=true&w=majority&appName=resumeshortlist';
 mongoose.connect(mongourl).then(()=>console.log("Database Connected")).catch(err=>{console.log(err)});
